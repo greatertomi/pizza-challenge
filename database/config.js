76f3c033f8abdf3ab481cb264c0873c8f9aacf83
Oshalusi John <oshalusijohn@gmail.com>
2021-02-10 11:55:22 +0100

    Added cleardb credentials

--- database/config.js
+++ database/config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pizzaHut'
+  host: 'us-cdbr-east-03.cleardb.com',
+  user: 'b0ed00455a9b8c',
+  password: 'd4de010a',
+  database: 'heroku_6b8254fed715126'
 };
+
+// CLEARDB_DATABASE_URL: mysql://b0ed00455a9b8c:d4de010a@us-cdbr-east-03.cleardb.com/heroku_6b8254fed715126?reconnect=true
+// git push heroku main
