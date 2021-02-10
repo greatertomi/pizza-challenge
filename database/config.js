99e1f7d66d524094601aaadc2441e59824d5fe67
Oshalusi John <oshalusijohn@gmail.com>
2021-02-10 12:48:29 +0100

    Final commit

--- database/config.js
+++ database/config.js
@@ -4,6 +4,3 @@ module.exports = {
   password: 'd4de010a',
   database: 'heroku_6b8254fed715126'
 };
-
-// CLEARDB_DATABASE_URL: mysql://b0ed00455a9b8c:d4de010a@us-cdbr-east-03.cleardb.com/heroku_6b8254fed715126?reconnect=true
-// git push heroku main
